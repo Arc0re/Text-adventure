6c6ed8f0a2d74ef6b6e53c108293d361788f6bab
Thomas Guyot <PaperboyCheero@yahoo.com>
2015-05-04 14:10:43 +0200

    Started the fight function and added classes stats variables.

--- functions.py
+++ functions.py
@@ -1,4 +1,10 @@
 #this module will contain some functions for the game.
 
+import variables
+
 #Pokemon style fight, the parameter is the enemy type (rat, spider... see globals)
 def combat(enemy):
+    damage = enemy[1]
+    print "An enemy appears! Its a wild %s!" %enemy
+    print "You have 3 choices."
+    action = raw_input("---------- Attack \n ---------- Cast \n ---------- Block \n")
--- variables.py
+++ variables.py
@@ -2,8 +2,16 @@
 name = ""
 skill = ""
 inventory = ["knife"]
+#Weapons and items
 knife = 5
 sword = 10
 mace = 15
 staff = 10
 equipped_weapon = ""
+#Classes stats
+_warrior = [10,0,20]
+_wizard = [8,20,10]
+_undead = [5,15,15]
+#Enemies : name, hp and attack points
+rat = [5,3]
+giant_spider = [8,5]
